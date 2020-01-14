12a937a1fbad754e5f5d4a84bc63122d76e36378
Fernando <fernando.calvo.sanz@gmail.com>
2020-01-14 21:01:29 +0000

    Create server

---
+++ src/server.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+const express = require("express");
+const bodyParser = require("body-parser")
+
+const postRobots = require("./controllers/post-robots");
+
+const API_PORT = process.env.API_PORT || 8080;
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/", postRobots);
+
+app.listen(API_PORT, () => console.log(`server listening at ${API_PORT}`));
